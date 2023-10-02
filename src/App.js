5e34ef32a64aa9da15a136a5dcbe3232a6b73448
AlbLaura <albornoz.laurasol@gmail.com>
2023-10-02 12:46:04 -0300

    Tema oscuro | Tema claro

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import GlobalStyle from "./GlobalStyle";
-
+import { temaClaro, temaOscuro } from "./Components/UI/temas";
+import { ThemeProvider } from "styled-components";
 import Container from "./Components/Container";
 import Header from "./Components/Header";
+import { BtnTema } from "./Components/UI";
+import SwitcherTema from "./Components/SwitcherTema"
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const toggleTema = () => {
+    setTema((tema) => !tema);
+  };
+
   return (
-    <>
+    <ThemeProvider theme={ tema ? temaClaro : temaOscuro }>
       <GlobalStyle />
+      <BtnTema onClick={toggleTema}>
+        <SwitcherTema tema={tema}/>
+      </BtnTema>
       <Header />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/Components/UI/variables.js
+++ src/Components/UI/variables.js
@@ -1,3 +1,11 @@
 export const colorPrimario = "#41D3BE";
+
+//tema claro
 export const fondoClaro = "#F1F1F1";
+export const contenidoClaro = "white";
 export const textoFondoClaro = "grey";
+
+//tema oscuro
+export const fondoOscuro = "#363537";
+export const contenidoOscuro = "#5C5B5E";
+export const textoFondoOscuro = "#FAFAFA";
